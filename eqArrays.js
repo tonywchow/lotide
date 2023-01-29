01d1ae01a8cf15299d5b71df982d6482d4b05815
Tony Chow <tchow9@gmail.com>
2023-01-29 12:21:15 -0800

    Modified eqArrays function with IF statement to check length of arrays

--- eqArrays.js
+++ eqArrays.js
@@ -6,7 +6,9 @@ const assertEqual = function(actual, expected) {
   }
 };
 const eqArrays = function(arr1, arr2) {
-  let result = '';
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
