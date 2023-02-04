42c6bc9d815d550e7e2150b516a7e1673327d24e
Tony Chow <tchow9@gmail.com>
2023-02-03 18:56:25 -0800

    Updated eqArray function with if length no equal

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -7,7 +7,9 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  let result = '';
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
@@ -15,7 +17,6 @@ const eqArrays = function(arr1, arr2) {
   }
   return true;
 } 
-
 const assertArraysEqual = function(arr1, arr2) {
 
   if (eqArrays(arr1, arr2) === true) {
