0ec20b012aed63e9180a4e29df931773d43dc436
Tony Chow <tchow9@gmail.com>
2023-02-14 20:46:10 -0800

    modulize the code by exporting function and separating test code

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
-//`
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
   }
 };
-  
+
+module.exports = assertEqual;
