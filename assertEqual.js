2d5f79cc27276c69999cd6a71b2b520926126762
Tony Chow <tchow9@gmail.com>
2023-02-15 17:57:37 -0800

    Adjusted code and introduced require and module.export to code

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -15,6 +8,8 @@ const eqArrays = function(arr1, arr2) {
     }
   }
   return true;
-} 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+};
+
+module.exports = eqArrays;
+
 
